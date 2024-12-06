3961a155421cf17b7cc0766be1bb744e4d616162
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-12-05 22:19:29 -0500

    Initial architecture setup

---
+++ backend/src/main/java/com/neu/knowledgeforge/KnowledgeforgeApplication.java
@@ -0,0 +1,13 @@
+package com.neu.knowledgeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeforgeApplication.class, args);
+	}
+
+}
